1417b0728bcc0501a8b4454d656c509e6e9ad506
Peter Kurczynski <pkurczynski@icloud.com>
2016-02-27 11:45:01 -0500

    redshift bin 2.5 < z <= 3.0

---
+++ sample04/run03b/analysis/sfr_vs_m_linfit_py/run_script.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+./sfr_100_vs_m_line_clip_sim_linexp.py -v -i 1000 -c 2.0 -z 2.73 candels_2015a_sample04_run03_speedymc_results_norejects_v1.fits candels_2015a_sample04_run03_sfr_100_vs_m_line_new_clip_sim_z27_median_ellipse.pdf > candels_2015a_sample04_run03_sfr_100_vs_m_line_new_clip_sim_z27_median_ellipse.log.txt
+./sfr_inst_vs_m_line_clip_sim_linexp.py -v -i 1000 -c 2.0 -z 2.73 candels_2015a_sample04_run03_speedymc_results_norejects_v1.fits candels_2015a_sample04_run03_sfr_inst_vs_m_line_new_clip_sim_z27_median_ellipse.pdf > candels_2015a_sample04_run03_sfr_inst_vs_m_line_new_clip_sim_z27_median_ellipse.log.txt
+./sfr_life_vs_m_line_clip_sim_linexp.py -v -i 1000 -c 2.0 -z 2.73 candels_2015a_sample04_run03_speedymc_results_norejects_v1.fits candels_2015a_sample04_run03_sfr_life_vs_m_line_new_clip_sim_z27_median_ellipse.pdf > candels_2015a_sample04_run03_sfr_life_vs_m_line_new_clip_sim_z27_median_ellipse.log.txt
+
